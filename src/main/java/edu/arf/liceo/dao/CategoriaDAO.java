60fb366cd6a4f2741a8f44485e403ba9cccf1c54
fonsitoo <alfregfont@gmail.com>
2025-12-10 10:54:05 +0100

    Implementacion de CategoriaDAO y cambio de version de java en el pom

---
+++ src/main/java/edu/arf/liceo/dao/CategoriaDAO.java
@@ -0,0 +1,73 @@
+package edu.arf.liceo.dao;
+
+import edu.arf.liceo.model.Categoria;
+import edu.arf.liceo.utils.conexionbd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaDAO {
+
+    public List<Categoria> listarTodas() {
+        List<Categoria> lista = new ArrayList<>();
+        String sql = "SELECT * FROM categoria";
+
+        try {
+            Connection con = conexionbd.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                lista.add(new Categoria(rs.getInt("id_categoria"), rs.getString("nombre")));
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public void guardarRelacionCategorias(int idItem, List<Categoria> categoriasSeleccionadas) {
+        String sql = "INSERT INTO item_categoria (id_item, id_categoria) VALUES (?, ?)";
+
+        try {
+            Connection con = conexionbd.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            for (Categoria cat : categoriasSeleccionadas) {
+                ps.setInt(1, idItem);
+                ps.setInt(2, cat.getIdCategoria());
+                ps.addBatch();
+            }
+
+            ps.executeBatch();
+            ps.close();
+            System.out.println("Categorías asociadas correctamente.");
+
+        } catch (SQLException e) {
+            System.err.println("Error al asociar categorías: " + e.getMessage());
+        }
+    }
+
+    public int obtenerUltimoIdItem() {
+        int id = 0;
+        String sql = "SELECT MAX(id_item) AS last_id FROM item";
+        try {
+            Connection con = conexionbd.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                id = rs.getInt("last_id");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+}
